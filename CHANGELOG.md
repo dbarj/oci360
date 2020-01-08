a0bf45eb9ad5b2d4b25c04d150dc1e5d35f0079c
Rodrigo Jorge <rodrigoaraujorge@hotmail.com>
2020-01-08 17:51:01 -0300

    Updating Version Number

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 ## Versions ##
 
+* 2002 (2020-01-13)
+  - Infrastructure Visual Design.
+
 * 2001 (2020-01-01)
   - Initial Version.
--- sql/00_software.sql
+++ sql/00_software.sql
@@ -19,8 +19,8 @@ DEF moat369_sw_logo_title_6 = ""
 DEF moat369_sw_logo_title_7 = ""
 DEF moat369_sw_logo_title_8 = ""
 
-DEF moat369_sw_vYYNN = 'v2001'
-DEF moat369_sw_vrsn  = '&&moat369_sw_vYYNN. (2020-01-01)'
+DEF moat369_sw_vYYNN = 'v2002'
+DEF moat369_sw_vrsn  = '&&moat369_sw_vYYNN. (2020-01-13)'
 
 DEF moat369_sw_copyright = 'oci360 (c) 2019, All rights reserved.'
 DEF moat369_sw_author    = 'rodrigo.jorge'
