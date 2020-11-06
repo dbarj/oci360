a4b3fe82c7382ce9c527e4f6eaea50f5e09405a4
Rodrigo Jorge <rodrigoaraujorge@hotmail.com>
2020-11-06 16:59:42 -0300

    Update oci360_oci_cli_access.sh

--- automation/oci360_oci_cli_access.sh
+++ automation/oci360_oci_cli_access.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # v1.0
+
 # Create oci-cli instance_principal access for OCI360
 # Only use this script if the tenancy you are running OCI360 is the same you want to get the info.
 
@@ -10,14 +11,14 @@ read v_instance_ocid
 [ -z "${v_instance_ocid}" ] && echo "Instance OCID can't be null." && exit 1
 
 v_dyngroup_def_name='OCI360_DG'
-echo -n "Dynamic Group Name [${v_dyngroup_def_name}]: "
+echo -n "Dynamic Group Name for OCI360 [${v_dyngroup_def_name}]: "
 read v_dyngroup_name
 [ -z "${v_dyngroup_name}" ] && v_dyngroup_name="${v_dyngroup_def_name}"
 
-v_dyngroup_def_name='OCI360_Policy'
-echo -n "Policy Name [${v_policy_def_name}]: "
+v_policy_def_name='OCI360_Policy'
+echo -n "Policy Name for OCI360 [${v_policy_def_name}]: "
 read v_policy_name
-[ -z "${v_policy_name}" ] && v_policy_name="${v_dyngroup_def_name}"
+[ -z "${v_policy_name}" ] && v_policy_name="${v_policy_def_name}"
 
 oci iam dynamic-group create \
 --name "${v_dyngroup_name}" \
