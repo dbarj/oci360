45453793222cb2b090cf13faff825c4101c0912a
Rodrigo Jorge <rodrigoaraujorge@hotmail.com>
2020-11-06 18:57:19 -0300

    Update oci360_oci_cli_access.sh

--- automation/oci360_oci_cli_access.sh
+++ automation/oci360_oci_cli_access.sh
@@ -3,6 +3,7 @@
 
 # Create oci-cli instance_principal access for OCI360
 # Only use this script if the tenancy you are running OCI360 is the same you want to get the info.
+# Run this script from Cloud Shell with Administrator account.
 
 set -eo pipefail
 
@@ -20,10 +21,33 @@ echo -n "Policy Name for OCI360 [${v_policy_def_name}]: "
 read v_policy_name
 [ -z "${v_policy_name}" ] && v_policy_name="${v_policy_def_name}"
 
-oci iam dynamic-group create \
+v_err=$(oci iam dynamic-group create \
 --name "${v_dyngroup_name}" \
 --matching-rule "instance.id = '${v_instance_ocid}'" \
---description 'Group to handle oci-cli calls from the host of OCI360.'
+--description 'Group to handle oci-cli calls from the host of OCI360.' 2>&1 >/dev/null) || true
+
+if grep -q -F 'EntityAlreadyExists' <<< "${v_err}"
+then
+  v_dyn_group_json=$(oci iam dynamic-group list --all | jq '.data[] | select(."name" == "'${v_dyngroup_name}'")')
+  v_dyn_group_id=$(jq -rc '.id' <<< "${v_dyn_group_json}")
+  v_dyn_group_rules=$(jq -rc '."matching-rule"' <<< "${v_dyn_group_json}")
+  v_new_dyn_group_rules="$(sed 's/}//' <<< "${v_dyn_group_rules}") , instance.id='${v_instance_ocid}'}"
+  if ! grep -q -F "${v_instance_ocid}" <<< "${v_new_dyn_group_rules}"
+  then
+    echo 'Dynamic Group already exists. Adding this instance on it.'
+    oci iam dynamic-group update \
+    --dynamic-group-id ${v_dyn_group_id} \
+    --force \
+    --matching-rule "${v_new_dyn_group_rules}"
+  else
+    echo 'Dynamic Group already exists with this tenancy on it.'
+  fi
+elif [ -n "${v_err}" ]
+then
+  echo "${v_err}"
+  echo "Unable to create ${v_dyngroup_name}."
+  exit 1
+fi
 
 v_tenancy_id=$(oci iam compartment list \
 --all \
@@ -33,7 +57,7 @@ v_tenancy_id=$(oci iam compartment list \
 --raw-output \
 --query "data[?contains(\"id\",'tenancy')].id | [0]")
 
-oci iam policy create \
+v_err=$(oci iam policy create \
 --compartment-id "${v_tenancy_id}" \
 --name "${v_policy_name}"  \
 --statements \
@@ -41,7 +65,19 @@ oci iam policy create \
   \"allow dynamic-group ${v_dyngroup_name} to read all-resources in tenancy\" ,
   \"allow dynamic-group ${v_dyngroup_name} to read usage-reports in tenancy\"
 ]" \
---description 'Policy to handle oci-cli calls from the host of OCI360.'
+--description 'Policy to handle oci-cli calls from the host of OCI360.' 2>&1 >/dev/null) || true
+
+if grep -q -F 'PolicyAlreadyExists' <<< "${v_err}"
+then
+  echo 'Policy already exists.'
+elif [ -n "${v_err}" ]
+then
+  echo "${v_err}"
+  echo "Unable to create ${v_dyngroup_name}."
+  exit 1
+fi
+
+echo "Script finished without any error."
 
 exit 0
 ####
