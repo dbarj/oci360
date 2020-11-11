9bdad2917a81cd9b5ea3aed4131b899874ed8ac0
Rodrigo Jorge <rodrigoaraujorge@hotmail.com>
2020-11-11 10:11:48 -0300

    Create remove_docker.sh

---
+++ container/remove_docker.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -x
+# v1.0
+
+set -eo pipefail
+set -x
+
+trap_err ()
+{
+  echo "Error on line $1 of \"remove_docker.sh\"."
+  exit 1
+}
+
+trap 'trap_err $LINENO' ERR
+
+# Directory Paths
+v_master_directory="/u01"
+v_db_dir="${v_master_directory}/oci360_database"
+v_apache_dir="${v_master_directory}/oci360_apache"
+
+# Container names
+v_oci360_con_name="oci360-tool"
+v_apache_con_name="oci360-apache"
+
+docker images
+docker ps
+
+userdel -r oci360 || true
+
+rm -rf "${v_db_dir}"
+
+docker stop ${v_oci360_con_name} || true
+docker rm ${v_oci360_con_name} || true
+
+rm -rf "${v_apache_dir}"
+mkdir -p "${v_apache_dir}"
+
+docker stop ${v_apache_con_name} || true
+docker rm ${v_apache_con_name} || true
+
+exit 0
+#####
